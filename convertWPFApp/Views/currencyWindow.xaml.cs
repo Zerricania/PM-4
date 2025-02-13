c6431da4f21a3d5e37450d4299f6e6a9895688b4
Zerricania <alisherkaa24@gmail.com>
2025-02-13 15:59:18 +0500

    промежуточно

---
+++ convertWPFApp/Views/currencyWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace convertWPFApp.Views;
+
+public partial class CurrencyWindow
+{
+    public CurrencyWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void ConvertButton_Click(object sender, RoutedEventArgs e)
+    {
+        double inputValue;
+        if (double.TryParse(InputTextBox.Text, out inputValue))
+        {
+            string? fromUnit = ((ComboBoxItem)FromComboBox.SelectedItem)?.Content.ToString();
+            string? toUnit = ((ComboBoxItem)ToComboBox.SelectedItem)?.Content.ToString();
+
+            if (fromUnit != null && toUnit != null)
+            {
+                double result = ConvertCurrency(inputValue, fromUnit, toUnit);
+                ResultTextBlock.Text = $"{inputValue} {fromUnit} = {result:F2} {toUnit}";
+            }
+            else
+            {
+                MessageBox.Show("Please select both currencies.");
+            }
+        }
+        else
+        {
+            MessageBox.Show("Please enter a valid number.");
+        }
+    }
+
+    private double ConvertCurrency(double value, string from, string to)
+    {
+        var exchangeRates = new Dictionary<string, double>
+        {
+            { "Tenge", 1 },       
+            { "Dollar", 470 },   
+            { "Ruble", 5.3 }      
+        };
+
+        if (exchangeRates.ContainsKey(from) && exchangeRates.ContainsKey(to))
+        {
+            double valueInKzt = value * exchangeRates[from]; 
+            return valueInKzt / exchangeRates[to]; 
+        }
+
+        return value; 
+    }
+}
