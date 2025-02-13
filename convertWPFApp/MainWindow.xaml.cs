ac023af15b6050a0a22af4ff57630b915ead6028
Zerricania <alisherkaa24@gmail.com>
2025-02-13 15:59:30 +0500

    г

---
+++ convertWPFApp/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using convertWPFApp.Views;
+namespace convertWPFApp;
+
+/// <summary>
+/// Interaction logic for MainWindow.xaml
+/// </summary>
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void currencyWindowOpen_Click(object sender, RoutedEventArgs e)
+    {
+        CurrencyWindow currencyWindow = new CurrencyWindow();
+        currencyWindow.ShowDialog(); 
+    }
+}
